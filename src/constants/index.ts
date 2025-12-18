2a3667bb419d1c4c3e509f7818cc3b64da42834b
jirateep12z <70580937+jirateep12z@users.noreply.github.com>
2025-12-18 15:08:43 +0700

    feat(*): initial commit

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export { DEFAULT_PRETTIER_CONFIG } from './builder.constants';
+export {
+  COMMON_IGNORE_PATTERNS,
+  FRAMEWORKS,
+  PRETTIER_OPTIONS_INFO
+} from './frameworks.constants';
+export {
+  IGNORE_PATTERNS_CATEGORY_ICONS,
+  IGNORE_PATTERNS_CATEGORY_LABELS,
+  IGNORE_PATTERNS_CATEGORY_ORDER
+} from './ignore-patterns.constants';
