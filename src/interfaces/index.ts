6a9ab71a9297f82bfb8be865908159c8e0b82266
jirateep12z <70580937+jirateep12z@users.noreply.github.com>
2025-12-18 15:08:43 +0700

    feat(*): initial commit

---
+++ src/interfaces/index.ts
@@ -0,0 +1,18 @@
+export type {
+  BuilderState,
+  GeneratedOutput,
+  PrettierConfigState
+} from './builder.interfaces';
+export type {
+  FrameworkSelectorProps,
+  IgnorePatternsEditorProps,
+  OutputPreviewProps,
+  PrettierConfigEditorProps
+} from './components.interfaces';
+export type { FrameworkIconProps } from './framework-icons.interfaces';
+export type {
+  FrameworkConfig,
+  InstallConfig,
+  PrettierConfig,
+  PrettierOverride
+} from './frameworks.interfaces';
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export type { FrameworkIconId, OutputFormat } from './builder.types';
+export type { CommonIgnoreCategory } from './common-ignore-category.types';
+export type { PackageManager } from './package-manager.types';
